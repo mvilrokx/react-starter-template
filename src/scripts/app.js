95d3fcd8d06e8c701dd1993a0c30cf0b7d35a59c
Mark Vilrokx <mvilrokx@gmail.com>
2015-03-24 20:01:59 -0700

    removed the if (module.hot) stuff, that is only necessary if you are using React from an external resource, if you use the npm version you don not need that

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -14,15 +14,6 @@ var features = [
   {name: 'Development and Production configuration and scripts', url: ''}
 ];
 
-var rootInstance = React.render(
+React.render(
   <WelcomeMsg features={features} />, document.getElementById('content')
 );
-
-if (module.hot) {
-  require('react-hot-loader/Injection').RootInstanceProvider.injectProvider({
-    getRootInstances: function () {
-      // Help React Hot Loader figure out the root component instances on the page:
-      return [rootInstance];
-    }
-  });
-}
